9734760f79cda2038df0d0cb82d34b55e9a76fb2
ncuevas <nicolas.cuevas@greencodesoftware.com>
2018-05-31 08:48:19 -0300

    Add documentation for iterator lists

--- lista.h
+++ lista.h
@@ -85,24 +85,52 @@ void lista_iterar(lista_t *lista, bool visitar(void *dato, void *extra), void *e
  ******************************************************************/
 
 
+// Se crea un iterador apuntando al principio de la lista. Si la lista
+// no posee elementos, entonces el iterador apunta al final (es decir, NULL).
+// Pre: La lista fue creada.
+// Post: Se devuelve un iterador apuntando al principio de la lista.
 lista_iter_t *lista_iter_crear(lista_t *lista);
 
 
+
+// Se devuelve true o false, dependiendo si el iterador esta posicionado al final o no.
+// Pre: El iterador fue creado.
 bool lista_iter_avanzar(lista_iter_t *iter);
 
 
+// Se devuelve un puntero a la posicion actual del iterador.
+// Pre: El iterador fue creado.
 void *lista_iter_ver_actual(const lista_iter_t *iter);
 
 
+// Se devuelve true o false dependiendo si el iterador esta al final o no.
+// Un iterador esta al final, cuando su posicion actual apunta a NULL.
+// Pre: El iterador fue creado.
 bool lista_iter_al_final(const lista_iter_t *iter);
 
 
+// Destruye el iterador.
+// Pre: El iterador fue creado.
+// Post: El iterador fue destruido.
 void lista_iter_destruir(lista_iter_t *iter);
 
 
+// Se devuelve true o false dependiendo si se pudo insertar o no en la lista. 
+// Al insertar un elemento, si el iterador no esta al final, este pasa a ser el actual,
+// y el pivot de actual anterior pasa a ser el siguiente del elemento insertado.
+// Si se inserta un elemento con un iterador apuntando al primer elemento de la lista, 
+// la operación es idéntica a llamar a lista_insertar_primero.
+// Si se inserta un elemento con un iterador apuntando al final,
+// la operación es idéntica a llamar a lista_insertar_ultimo.
+// Pre: El iterador fue creado.
+// Post: Se agrego un elemento, y la lista aumenta en 1 su tamanio.
 bool lista_iter_insertar(lista_iter_t *iter, void *dato);
 
 
+// Se devuelve un puntero a la posicion actual antes de ser borrada. Si la lista
+// esta vacia, devuelve NULL.
+// Pre: El iterador fue creado.
+// Post: Si Se elimina un elemento de la lista, se reduce su tamanio en 1.
 void *lista_iter_borrar(lista_iter_t *iter);
 
 
